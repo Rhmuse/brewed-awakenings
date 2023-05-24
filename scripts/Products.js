fa652ccb8c90f7aa7bfbd82933fcd25523662d14
Rhett Muse <rhett.muse123@gmail.com>
2023-05-24 11:16:22 -0500

    add eventlistner to display price

--- scripts/Products.js
+++ scripts/Products.js
@@ -2,6 +2,30 @@ import { getProducts } from "./database.js"
 
 const products = getProducts()
 
+document.addEventListener(
+    "click",
+    e => {
+        const itemClicked = e.target;
+
+        if (itemClicked.id.startsWith("product")) {
+            const [, productPrimaryKey] = itemClicked.id.split("--")
+
+            let matchingProduct = null;
+            for (const product of products) {
+                if (product.id === parseInt(productPrimaryKey)) {
+                    matchingProduct = product;
+                }
+            }
+
+            if (matchingProduct !== null) {
+                window.alert(`${matchingProduct.name} cost $${matchingProduct.price}.`)
+            } else {
+                window.alert("Product not found!")
+            }
+        }
+    }
+)
+
 export const Products = () => {
     let html = "<ul>"
 
